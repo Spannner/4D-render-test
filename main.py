2c3d504c616b460597d585f8f020c24ad15199ce
Spanner <hanthony28n@gmail.com>
2020-01-26 12:11:31 +0000

    Added drawing

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import pygame
 import numpy as n
 
+projection = ( # 3d -> 2d projection matrix
+    (1,0,0),
+    (0,1,0)
+)
+
 def generate_square(dimensions):
     vertices = []
     for i in range(0,2**dimensions):
         vertices.append(n.array(str(bin(i))[2:].zfill(dimensions).replace('',',').replace('0','-1')[1:-1].split(','),dtype='int'))
     return vertices
+
+def drawVertex(pos,screen):
+    pos = (int(50*pos[0])+400,int(50*pos[1])+400)
+    pygame.draw.circle(screen,(255,255,255),pos,3)
+
+def draw():
+    screen.fill((0,0,0))
+    projectedPoints = []
+    for vertex in cube:
+        projected = n.dot(projection,vertex)
+        projectedPoints.append(projected)
+        drawVertex(projected,screen)
+
 
 def main():
     while True:
@@ -13,7 +31,7 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            #draw
+            draw()
             pygame.display.flip()
             pygame.time.wait(10)
 pygame.init()
